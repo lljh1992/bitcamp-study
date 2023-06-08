0f2ac3a326f7e17cbffecf2dddf53ee0dc19f43c
lljh <lljh1992@naver.com>
2023-06-08 13:55:48 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -1,17 +1,13 @@
 package bitcamp.myapp.handler;
 
-import javax.swing.text.PasswordView;
-
 import bitcamp.util.Prompt;
+import bitcamp.myapp.vo.Member;
 
 public class MemberHandler {
 
   static final int MAX_SIZE = 100;
-  static int[] no = new int[MAX_SIZE];
-  static String[] name = new String[MAX_SIZE];
-  static String[] email = new String[MAX_SIZE];
-  static String[] password = new String[MAX_SIZE];
-  static char[] gender = new char[MAX_SIZE];
+  static Member[] members = new Member[MAX_SIZE];
+
   static int userId = 1;
   static int length = 0;
 
@@ -23,13 +19,19 @@ public class MemberHandler {
       System.out.println("더이상 입력할 수 없습니다!");
       return;
     }
-    name[length] = Prompt.inputString("이름? ");
-    email[length] = Prompt.inputString("이메일? ");
-    password[length] = Prompt.inputString("암호? ");
-    gender[length] = inputGender((char)0);
+
+    Member m = new Member();
+
+    m.name = Prompt.inputString("이름? ");
+    m.email = Prompt.inputString("이메일? ");
+    m.password = Prompt.inputString("암호? ");
+    m.gender = inputGender((char)0);
+    m.no = userId++;
+
+    // 위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게
+    // 레퍼런스 배열에 담는다.
+    members[length++] = m;
     
-    no[length] = userId++;
-    length++;
   }
 
   public static void printMembers() {
@@ -38,23 +40,21 @@ public class MemberHandler {
     System.out.println("---------------------------------------");
 
     for (int i = 0; i < length; i++) {
-      System.out.printf("%d, %s, %s, %s\n",
-      no[i], name[i], email[i],
-      toGenderString(gender[i]));
+      Member m = members[i];
+      System.out.printf("%d, %s, %s, %s\n", 
+        m.no, m.name, m.email, 
+        toGenderString(m.gender));
     }
   }
-  public static void viewMember() {
-    String memberNo = Prompt.inputString(("번호? "));
-    // 입력 받은 번호를 가지고 배열에서 해당 회원을 찾아야 한다.
-    for (int i = 0; i < length; i++){
-      if (no[i] == Integer.parseInt(memberNo)) {
-        System.out.printf("이름: %s\n", name[i]);
-        System.out.printf("이메일: %s\n", email[i]);
-        System.out.printf("성별: %s\n", toGenderString(gender[i]));
-        
-        // System.out.printf("성별: %s\n", gender[i] == 'M' ? "남성" : "여성");
 
-        // i 번째 항목에 저장된 회원 정보 출력
+  public static void viewMember() {
+    String memberNo = Prompt.inputString("번호? ");
+    for (int i = 0; i < length; i++) {
+      Member m = members[i];
+      if (m.no == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", m.name);
+        System.out.printf("이메일: %s\n", m.email);
+        System.out.printf("성별: %s\n", toGenderString(m.gender));
         return;
       }
     }
@@ -64,76 +64,33 @@ public class MemberHandler {
   public static String toGenderString(char gender) {
     return gender == 'M' ? "남성" : "여성";
   }
-  
-
-  private static boolean available() {
-    return length < MAX_SIZE;
-  }
 
   public static void updateMember() {
     String memberNo = Prompt.inputString("번호? ");
-    for (int i =0; i < length; i++){
-      if (no[i] == Integer.parseInt(memberNo)){
-      System.out.printf("이름(%s)? ", name[i]);
-      name[i] = Prompt.inputString(" ");
-      System.out.printf("이메일(%s)? ", email[i]);
-      email[i] = Prompt.inputString(" ");
-      System.out.printf("새암호? ");
-      password[i] = Prompt.inputString(" ");
-      gender[i] = inputGender(gender[i]);
-      System.out.printf("성별(%s)? ", toGenderString(gender[i]));
-      return;
-      
-    }
-  }
-}
-
-  public static void delMember() {
-    int memberNo = Integer.parseInt(Prompt.inputString("번호? "));
-    
-    int deletedIndex = indexOf(memberNo);
-    if (deletedIndex == -1) {
-      System.out.println("해당 번호의 회원이 없습니다!");
-      return;
-    }
-
-    for (int i = deletedIndex; i < length -1; i++){
-      no[i] = no[i +1];
-      name[i] = name[i +1];
-      email[i] = email[i +1];
-      password[i] = password[i +1];
-      gender[i] = gender[i +1];
-    }
-      
-    no[length -1] = 0;
-    name[length -1] = null;
-    email[length -1] = null;
-    password[length -1] = null;
-    gender[length -1] = (char)0;
-    
-  
-    length--;
-  }
-  
-  private static int indexOf(int memberNo) {
-    for (int i = 0; i < length; i++){
-      if (no[i] == memberNo) {
-        return i;
+    for (int i = 0; i < length; i++) {
+      Member m = members[i];
+      if (m.no == Integer.parseInt(memberNo)) {
+        System.out.printf("이름(%s)? ", m.name);
+        m.name = Prompt.inputString("");
+        System.out.printf("이메일(%s)? ", m.email);
+        m.email = Prompt.inputString("");
+        System.out.printf("새암호? ");
+        m.password = Prompt.inputString("");
+        m.gender = inputGender(m.gender);
+        return;
       }
     }
-    return -1;
+    System.out.println("해당 번호의 회원이 없습니다!");
   }
-
-
 
   private static char inputGender(char gender) {
     String label;
     if (gender == 0) {
       label = "성별?\n";
     } else {
-      label = String.format("성별(%s)?\n" + toGenderString(gender));
+      label = String.format("성별(%s)?\n", toGenderString(gender));
     }
-      while (true) {
+    while (true) {
       String menuNo = Prompt.inputString(label + 
       "  1. 남자\n" + 
       "  2. 여자\n" + 
@@ -146,17 +103,38 @@ public class MemberHandler {
           return FEMALE;
         default:
           System.out.println("무효한 번호입니다.");
-    
       }
-       
     }
   }
+
+  public static void deleteMember() {
+    int memberNo = Prompt.inputInt("번호? ");
+
+    int deletedIndex = indexOf(memberNo);
+    if (deletedIndex == -1) {
+      System.out.println("해당 번호의 회원이 없습니다!");
+      return;
+    }
+
+    for (int i = deletedIndex; i < length - 1; i++) {
+      members[i] = members[i + 1];
+
+    }
+
+    members[--length] = null;
+  }
+
+  private static int indexOf(int memberNo) {
+    for (int i = 0; i < length; i++) {
+      Member m = members[i];
+      if (m.no == memberNo) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  private static boolean available() {
+    return length < MAX_SIZE;
+  }
 }
-
-
-
- 
-
-
-
-
