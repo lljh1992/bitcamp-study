7eeeb718e2a310eeeab55dbd6766b4afe86a96f1
lljh1992 <lljh1992@naver.com>
2023-06-28 16:27:11 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/Member.java
@@ -2,7 +2,7 @@ package bitcamp.myapp.vo;
 
 public class Member {
 
-  private static int userId = 1;
+  public static int userId = 1;
 
   public static final char MALE = 'M';
   public static final char FEMALE = 'W';
@@ -32,6 +32,7 @@ public class Member {
   // 그렇게 하기 위해 수퍼 클래스의 equals()를 재정의 한다.
   // => 이것을 "오버라이딩(overriding)"이라 부른다.
   //
+  @Override
   public boolean equals(Object obj) {
     if (obj == null) {
       return false;
@@ -51,21 +52,21 @@ public class Member {
       return false;
     }
 
-    //    if (this.getName() != null && !this.getName().equals(m.getName())) {
-    //      return false;
-    //    }
+    // if (this.getName() != null && !this.getName().equals(m.getName())) {
+    // return false;
+    // }
     //
-    //    if (this.getEmail() != null && !this.getEmail().equals(m.getEmail())) {
-    //      return false;
-    //    }
+    // if (this.getEmail() != null && !this.getEmail().equals(m.getEmail())) {
+    // return false;
+    // }
     //
-    //    if (this.getPassword() != null && !this.getPassword().equals(m.getPassword())) {
-    //      return false;
-    //    }
+    // if (this.getPassword() != null && !this.getPassword().equals(m.getPassword())) {
+    // return false;
+    // }
     //
-    //    if (this.getGender() != m.getGender()) {
-    //      return false;
-    //    }
+    // if (this.getGender() != m.getGender()) {
+    // return false;
+    // }
 
     return true;
   }
@@ -73,30 +74,39 @@ public class Member {
   public int getNo() {
     return no;
   }
+
   public void setNo(int no) {
     this.no = no;
   }
+
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   public String getEmail() {
     return email;
   }
+
   public void setEmail(String email) {
     this.email = email;
   }
+
   public String getPassword() {
     return password;
   }
+
   public void setPassword(String password) {
     this.password = password;
   }
+
   public char getGender() {
     return gender;
   }
+
   public void setGender(char gender) {
     this.gender = gender;
   }
