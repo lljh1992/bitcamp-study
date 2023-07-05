276d9bcc9295d8bfc9affa2e8a6f3309dc201a3b
lljh1992 <lljh1992@naver.com>
2023-07-05 16:24:10 +0900

    .

---
+++ myapp/app-client/src/main/java/bitcamp/myapp/ClientApp.java
@@ -0,0 +1,33 @@
+package bitcamp.myapp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class ClientApp {
+
+  public static void main(String[] args) throws Exception {
+    if (args.length < 1) {
+      System.out.println("실행 예) java -cp bin/main bitcamp.myapp.ClientApp 서버주소");
+      // 192.168.0.20
+      return;
+    }
+    Socket socket = new Socket(args[0], 8888);
+
+    OutputStream out0 = socket.getOutputStream();
+    DataOutputStream out = new DataOutputStream(out0);
+
+    InputStream in0 = socket.getInputStream();
+    DataInputStream in = new DataInputStream(in0);
+
+    out.writeUTF("안녕! Hello");
+
+    out.close();
+    in.close();
+    socket.close();
+  }
+}
+
+
---
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -0,0 +1,33 @@
+package bitcamp.myapp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+
+  public static void main(String[] args) throws Exception {
+    ServerSocket serverSocket = new ServerSocket(8888);
+    System.out.println("서버 실행 중...");
+
+    Socket socket = serverSocket.accept();
+
+    InputStream in0 = socket.getInputStream();
+    DataInputStream in = new DataInputStream(in0);
+
+    OutputStream out0 = socket.getOutputStream();
+    DataOutputStream out = new DataOutputStream(out0);
+
+    System.out.println(in.readUTF());
+
+    in.close();
+    out.close();
+    socket.close();
+    serverSocket.close();
+  }
+}
+
+
