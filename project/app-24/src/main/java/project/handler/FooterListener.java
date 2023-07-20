ee9201c1d473dc8420d97d8bbb0fa698e8f9da0b
lljh1992 <lljh1992@naver.com>
2023-07-20 09:35:39 +0900

    24번

---
+++ project/app-24/src/main/java/project/handler/FooterListener.java
@@ -0,0 +1,11 @@
+package project.handler;
+
+import project.util.ActionListener;
+import project.util.BreadcrumbPrompt;
+
+public class FooterListener implements ActionListener {
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    System.out.println("Copyright \u00a9 by 네클7기------------------------------------");
+  }
+}
