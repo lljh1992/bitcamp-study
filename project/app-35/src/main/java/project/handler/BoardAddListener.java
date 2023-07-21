e2656cb8378c06c2166eb00e7d67b46edcc58d5e
lljh1992 <lljh1992@naver.com>
2023-07-21 09:36:09 +0900

    .

---
+++ project/app-35/src/main/java/project/handler/BoardAddListener.java
@@ -0,0 +1,23 @@
+package project.handler;
+
+import java.util.List;
+import project.util.BreadcrumbPrompt;
+import project.vo.Board;
+
+public class BoardAddListener extends AbstractBoardListener {
+
+  public BoardAddListener(List<Board> list) {
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    Board board = new Board();
+    board.setTitle(prompt.inputString("제목? "));
+    board.setContent(prompt.inputString("내용? "));
+    board.setWriter(prompt.inputString("작성자? "));
+    board.setPassword(prompt.inputString("암호? "));
+
+    this.list.add(board);
+  }
+}
---
+++ project/app-35/src/main/java/project/handler/MemberDetailListener.java
@@ -0,0 +1,32 @@
+package project.handler;
+
+import java.util.List;
+import project.util.BreadcrumbPrompt;
+import project.vo.Member;
+
+public class MemberDetailListener extends AbstractMemberListener {
+
+
+  public MemberDetailListener(List<Member> list) {
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    int memberNo = prompt.inputInt("번호?");
+
+    Member m = this.findBy(memberNo);
+    if (m == null) {
+      System.out.println("해당 번호의 회원이 없습니다!");
+      return;
+    }
+    System.out.printf("동: %s\n", m.getA());
+    System.out.printf("호수: %s\n", m.getB());
+    System.out.printf("이름: %s\n", m.getName());
+    System.out.printf("H.P: %s\n", m.getPhonenumber());
+    System.out.printf("차량 번호: %s\n", m.getCarnumber());
+    System.out.printf("차량 보유 현황: %s\n", m.getVehicleOwnership());
+    System.out.printf("거주 여부: %s\n", m.getType());
+  }
+
+}
