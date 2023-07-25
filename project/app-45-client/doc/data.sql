e58ac6412bc181296c846c78830e3cd2557b09d3
lljh1992 <lljh1992@naver.com>
2023-07-25 16:57:50 +0900

    .

---
+++ project/app-45-client/doc/data.sql
@@ -0,0 +1,31 @@
+-- project_newmember 테이블 예제 데이터
+insert into project_newmember(newmember_no, newid, newpassword, newname, newphonenumber)
+  value(1, '1111', '1111', '홍길동', '010-1111-1111');
+  insert into project_newmember(newmember_no, newid, newpassword, newname, newphonenumber)
+  value(2, '2222', '3333', '임꺽정', '010-2222-2222');
+  insert into project_newmember(newmember_no, newid, newpassword, newname, newphonenumber)
+  value(3, '3333', '3333', '이순신', '010-3333-3333');
+
+-- project_memnber 테이블 예제 데이터
+insert into project_member(member_no, building, unit, name, phonenumber, carnumber, VehiceOwnership, residencestatus)
+  value(1, '108', '1704', '홍길동', '010-1111-1111', '11가1111', '1', 'Y');
+insert into project_member(member_no, building, unit, name, phonenumber, carnumber, VehiceOwnership, residencestatus)
+  value(2, '101', '1102', '임꺽정', '010-2222-2222', '22가2222', '2', 'N');
+insert into project_member(member_no, building, unit, name, phonenumber, carnumber, VehiceOwnership, residencestatus)
+  value(3, '104', '1001', '이순신', '010-3333-3333', '33가3333', '2', 'Y');
+  
+-- project_board 테이블 예제 데이터
+insert into project_board(board_no, title, content, writer, password, category)
+  values(1, '민원사항1', '내용1', '홍길동', '1111', 1);
+insert into project_board(board_no, title, content, writer, password, category)
+  values(2, '민원사항3', '내용2', '임꺽정', '1111', 1);
+insert into project_board(board_no, title, content, writer, password, category)
+  values(3, '민원사항2', '내용3', '이순신', '1111', 1);
+
+-- project_notice 테이블 예제 데이터
+insert into project_board(board_no, title, content, writer, password, category)
+  values(11, '공지사항1', '내용1', '관리자1', '1111', 2);
+insert into project_board(board_no, title, content, writer, password, category)
+  values(12, '공지사항2', '내용2', '관리자2', '1111', 2);
+insert into project_board(board_no, title, content, writer, password, category)
+  values(13, '공지사항3', '내용3', '관리자3', '1111', 2);
---
+++ project/app-client/doc/ddl.sql
@@ -0,0 +1,50 @@
+drop table project_board;
+drop table project_member;
+drop table project_newmember;
+
+create table project_newmember(
+  newmember_no int not null,
+  newid varchar(20) not null,
+  newpassword varchar(100) null,
+  newname varchar(20) not null,
+  newphonenumber varchar(20) not null
+);
+
+alter table project_newmember
+  add constraint primary key(newmember_no),
+  modify column newmember_no int not null auto_increment;
+
+create table project_member(
+  member_no int not null,
+  building varchar(4) not null,
+  unit varchar(4) not null,
+  name varchar(20) not null,
+  phonenumber varchar(20) not null,
+  carnumber varchar(15) not null,
+  VehiceOwnership varchar(2) default null,
+  residencestatus char(1) not null
+  entryTimes dateTime,
+  exitTimes dateTime
+);
+
+alter table project_member
+  add constraint primary key(member_no),
+  modify column member_no int not null auto_increment;
+
+create table project_board(
+  board_no int not null,
+  title varchar(255) not null,
+  content text null,
+  writer varchar(20) not null,
+  password varchar(100) null,
+  view_count int default 0,
+  created_date datetime default now()
+);
+
+-- 게시판에 카테고리 컬럼 추가
+alter table project_board
+  add column category int not null;
+  
+-- 차량관리에 입차 출사 컬럼 추가
+alter table project_member
+  add column 
