0d4e11f414036c8ca65dfb6e7e55fe8e1cc27b6e
lljh1992 <lljh1992@naver.com>
2023-08-09 19:18:04 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardListServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardListServlet.java
@@ -5,19 +5,20 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import bitcamp.myapp.vo.Board;
-import bitcamp.util.AbstractServlet;
 
 @WebServlet("/board/list")
-public class BoardListServlet extends AbstractServlet {
+public class BoardListServlet extends HttpServlet {
 
+  private static final long serialVersionUID = 1L;
   SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
 
   @Override
-  public void service(ServletRequest request, ServletResponse response)
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
     int category = Integer.parseInt(request.getParameter("category"));
@@ -44,20 +45,12 @@ public class BoardListServlet extends AbstractServlet {
 
     out.println("<tbody>");
     for (Board board : list) {
-      out.printf("<tr>"
-          + " <td>%d</td>"
-          + " <td><a href='/board/detail?category=%d&no=%d'>%s</a></td>"
-          + " <td>%s</td>"
-          + " <td>%d</td>"
-          + " <td>%s</td></tr>\n",
-          board.getNo(),
-          board.getCategory(),
-          board.getNo(),
-          (board.getTitle().length() > 0 ? board.getTitle() : "제목없음"),
-          board.getWriter().getName(),
-          board.getViewCount(),
-          dateFormatter.format(board.getCreatedDate())
-          );
+      out.printf(
+          "<tr>" + " <td>%d</td>" + " <td><a href='/board/detail?category=%d&no=%d'>%s</a></td>"
+              + " <td>%s</td>" + " <td>%d</td>" + " <td>%s</td></tr>\n",
+          board.getNo(), board.getCategory(), board.getNo(),
+          (board.getTitle().length() > 0 ? board.getTitle() : "제목없음"), board.getWriter().getName(),
+          board.getViewCount(), dateFormatter.format(board.getCreatedDate()));
     }
     out.println("</tbody>");
     out.println("</table>");
@@ -67,14 +60,5 @@ public class BoardListServlet extends AbstractServlet {
   }
 
 }
-
-
-
-
-
-
-
-
-
 
 
--- myapp/app-server/src/main/java/bitcamp/myapp/handler/InitServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/InitServlet.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -14,14 +15,12 @@ import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.dao.MySQLBoardDao;
 import bitcamp.myapp.dao.MySQLMemberDao;
-import bitcamp.util.AbstractServlet;
 import bitcamp.util.SqlSessionFactoryProxy;
 
-@WebServlet(
-    value="/init",
-    loadOnStartup = 1
-    )
-public class InitServlet extends AbstractServlet {
+@WebServlet(value = "/init", loadOnStartup = 1)
+public class InitServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
 
   public static SqlSessionFactory sqlSessionFactory;
   public static BoardDao boardDao;
@@ -33,9 +32,8 @@ public class InitServlet extends AbstractServlet {
     System.out.println("InitServlet.init() 호출됨!");
 
     try {
-      sqlSessionFactory = new SqlSessionFactoryProxy(
-          new SqlSessionFactoryBuilder().build(
-              Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml")));
+      sqlSessionFactory = new SqlSessionFactoryProxy(new SqlSessionFactoryBuilder()
+          .build(Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml")));
 
       boardDao = new MySQLBoardDao(sqlSessionFactory);
       memberDao = new MySQLMemberDao(sqlSessionFactory);
@@ -47,8 +45,9 @@ public class InitServlet extends AbstractServlet {
   }
 
   @Override
-  public void service(ServletRequest request, ServletResponse response)
+  protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
     out.println("<!DOCTYPE html>");
